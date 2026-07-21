3f1ea6a0b75e1119ea8d5acf9e4e23f51aa688d9
PETRAU Guillaume <59561327+stagekozi@users.noreply.github.com>
2026-07-21 15:32:16 +0200

    test(titles): add unit tests for domain behavior and create handler

---
+++ tests/Mediaspot.UnitTests/TitleTests.cs
@@ -0,0 +1,43 @@
+﻿using Mediaspot.Domain.Titles;
+using Shouldly;
+
+namespace Mediaspot.UnitTests
+{
+    public class TitleTests
+    {
+
+        [Fact]
+        public void Create_Should_Throw_When_Name_Is_Empty()
+        {
+            var action = () => Title.Create(
+                string.Empty,
+                "Description",
+                new DateOnly(2024, 3, 1),
+                TitleType.Movie);
+
+            Should.Throw<ArgumentException>(action);
+        }
+
+        [Fact]
+        public void Update_Should_Update_Title_Properties()
+        {
+            var title = Title.Create(
+                "Star Wars",
+                "Original description",
+                new DateOnly(1977, 5, 25),
+                TitleType.Movie);
+
+            title.Update(
+                "Star Wars: A New Hope",
+                "Updated description",
+                new DateOnly(1977, 5, 25),
+                TitleType.Movie);
+
+            title.Name.ShouldBe("Star Wars: A New Hope");
+            title.Description.ShouldBe("Updated description");
+            title.ReleaseDate.ShouldBe(new DateOnly(1977, 5, 25));
+            title.Type.ShouldBe(TitleType.Movie);
+        }
+
+    }
+}
