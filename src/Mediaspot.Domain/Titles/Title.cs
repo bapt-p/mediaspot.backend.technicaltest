eeb6a856b957989909443408131ed1fb302de9e9
PETRAU Guillaume <59561327+stagekozi@users.noreply.github.com>
2026-07-21 17:18:54 +0200

    feat(titles): add title domain model and invariants

---
+++ src/Mediaspot.Domain/Titles/Title.cs
@@ -0,0 +1,64 @@
+﻿using Mediaspot.Domain.Common;
+
+namespace Mediaspot.Domain.Titles
+{
+    public class Title : AggregateRoot
+    {
+        public string Name { get; private set; } = string.Empty;
+
+        public string? Description { get; private set; }
+
+        public DateOnly? ReleaseDate { get; private set; }
+
+        public TitleType Type { get; private set; }
+
+        // Private constructor to prevent uncontrolled instanciation
+        private Title()
+        {
+        }
+
+        // Factory method used to create a valid title instance,
+        // centralise entity initialisation and ensure domain invariance are enforced a creation time 
+        public static Title Create(
+          string name,
+          string? description,
+          DateOnly? releaseDate,
+          TitleType type)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException(
+                    "Title name cannot be empty.",
+                    nameof(name));
+            }
+
+            return new Title
+            {
+                Name = name.Trim(),
+                Description = description,
+                ReleaseDate = releaseDate,
+                Type = type
+            };
+        }
+
+        // Update the state of title while preserving domain invariance 
+        public void Update(
+        string name,
+        string? description,
+        DateOnly? releaseDate,
+        TitleType type)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException(
+                    "Title name cannot be empty.",
+                    nameof(name));
+            }
+
+            Name = name.Trim();
+            Description = description;
+            ReleaseDate = releaseDate;
+            Type = type;
+        }
+    }
+}
---
+++ src/Mediaspot.Domain/Titles/TitleType.cs
@@ -0,0 +1,9 @@
+﻿namespace Mediaspot.Domain.Titles
+{
+    public enum TitleType
+    {
+        Movie = 1,
+        Series = 2,
+        Other = 3
+    }
+}
