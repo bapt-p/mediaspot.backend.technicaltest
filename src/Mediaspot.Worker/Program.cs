08f5c12e503302388c59497f2eaa07fc1f564425
Hadesjoueur5 . <rapaceduchaos012@sfr.fr>
2026-07-23 15:18:22 +0200

    feat(worker): process pending transcode jobs

---
+++ src/Mediaspot.Worker/Program.cs
@@ -0,0 +1,14 @@
+﻿using Mediaspot.Infrastructure;
+using Mediaspot.Application;
+using Mediaspot.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddInfrastructure("Mediaspot.Backend.TechnicalTest");
+
+builder.Services.AddHostedService<TranscodeWorker>();
+
+IHost host = builder.Build();
+
+host.Run();
